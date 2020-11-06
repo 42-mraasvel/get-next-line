d0e7b696626a387f48fd4228a5c23c318ee6784e
mraasvel <mbraasveldt@gmail.com>
2020-11-06 22:35:41 +0100

    weird

---
+++ last_line_empty/get_next_line.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   get_next_line.h                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: mraasvel <mraasvel@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/11/03 13:17:56 by mraasvel      #+#    #+#                 */
+/*   Updated: 2020/11/06 20:48:13 by mraasvel      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stddef.h>
+
+# define BUFFER_SIZE 1
+
+typedef struct		s_buffer
+{
+	char			*buffer;
+	size_t			position;
+	size_t			line_size;
+	int				fd;
+	struct s_buffer	*next;
+}					t_buffer;
+
+int					get_next_line(int fd, char **line);
+
+/*
+** utility functions
+*/
+
+void				*ft_memcpy(void *dest, const void *src, size_t n);
+int					add_fd_to_lst(t_buffer **start, int fd);
+int					del_fd_from_list(t_buffer **start, int fd);
+int					ft_clear_lst(t_buffer **start);
+
+#endif
